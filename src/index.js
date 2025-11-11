ed649417ed8af37ebd39b5ab264dbaef238490a0
khansax <162153156+khansax@users.noreply.github.com>
2025-11-10 18:07:43 -0600

    perubahan coding

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const routes = require('../routes');
+const routes = require('./routes');
 
 app.use(express.json());
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(Server running on port ${PORT});
 });
-
